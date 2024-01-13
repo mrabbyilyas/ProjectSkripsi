3a6ba4ac432d1a84171c33627827f9a59164da9d
rbbylys <muhammad.ilyas004@binus.ac.id>
2024-01-13 19:58:26 +0700

    Initial Commit

---
+++ Assets/_Script/Trackable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trackable : DefaultObserverEventHandler
+{
+    public AudioSource suara;
+    void Awake()
+    {
+        suara = GetComponent<AudioSource>();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        suara.Play();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        suara.Stop();
+    }
+}
---
+++ Assets/_Script/buttonmanager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonmanager : MonoBehaviour
+{
+    public void ChangeScene(string NamaScene)
+    {
+        SceneManager.LoadScene(NamaScene);
+        Debug.Log("berhasil pindah scene");
+    }
+
+    public void onExitClicked()
+    {
+        Application.Quit();
+        Debug.Log("Keluar aplikasi");
+    }
+
+    private void onAndroidBackButtonClick()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Homescreen", LoadSceneMode.Single);
+        }
+    }
+}
